5b5fd3f4cbbf97d0c7c6231aaa3f0c2dd9c924a3
Camille <camille.ragland@gmail.com>
2024-07-03 17:35:35 -0400

    Finished Javascript Practice

--- Javascript/Lesson-2/javascript.js
+++ Javascript/Lesson-2/javascript.js
@@ -1,2 +1,56 @@
 const string = "The revolution will not be televised";
-console.log(string);
+const badString = string;
+console.log(badString);
+
+const single = 'single quotes';
+const double = "double quotes";
+const backTick = `backtick`;
+console.log(single);
+console.log(double);
+console.log(backTick);
+
+const name1 = "Chris";
+const greeting = `Hello, ${name1}`;
+console.log(greeting);
+
+const one = "Hello, ";
+const two = "How are you?"
+const joined = `${one}${two}`;
+console.log(joined);
+
+const greeting1 = "Hello";
+const name2 = "Carl";
+console.log(greeting1 + ", " + name2);
+
+const song = "Fight the Youth";
+const score = 9;
+const highestScore = 10
+const output = `I like the song ${song}. I gave it a score of ${(score/highestScore) * 100}%.`;
+console.log(output);
+
+const newLine = `One day you finally knew
+what you had to do, and began,`;
+console.log(newLine);
+
+const newLine1 = "Pne day you finally knew\nwhat you had to do, and began,";
+console.log(newLine1);
+
+const goodQuotes = 'She said "I think so!"';
+const goodQuotes1 = `She said "I'm not going in there!"`;
+console.log(goodQuotes);
+console.log(goodQuotes1);
+
+const bigmouth = 'I\'ve got no right to take my place...';
+console.log(bigmouth);
+
+const name3 = "Front ";
+const number = 242;
+console.log(name3 + number);
+
+const myString = "123";
+const myNum = Number(myString);
+console.log(typeof myNum);
+
+const myNum1 = 123;
+const myString1 = String(myNum1);
+console.log(typeof myString2);
