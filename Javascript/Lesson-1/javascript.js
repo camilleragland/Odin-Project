99ad0848f45e213353096d94f4d122f036933d46
Camille <camille.ragland@gmail.com>
2024-07-03 15:06:08 -0400

    Started Javascript Oden Project Lessons

---
+++ Javascript/Lesson-1/javascript.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 7 * a;
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual/MAX;
+
+
+console.log(23 + 97);
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+console.log((4 + 6 + 9) / 77);
+console.log(a);
+console.log(9 * a);
+console.log(b);
+console.log(percentage); 
---
+++ Javascript/Lesson-2/javascript.js
@@ -0,0 +1,2 @@
+const string = "The revolution will not be televised";
+console.log(string);
